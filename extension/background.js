888e90611364153ba3082cb144f6f0b2ffeff9f7
Marinescu Dragos <marinescudragos2014@gmail.com>
2025-11-02 16:46:54 +0200

    blank lines removed

--- extension/background.js
+++ extension/background.js
@@ -1,15 +1,12 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        
         if (request.action === "GCA_PROCESS_INPUT") {
-            
             console.log("Background Script: Message received from:", sender.tab.url);
             console.log("Background Script: Data received:", request.data);
             sendResponse({ 
                 status: "received", 
                 message: "Message received and is now being processed." 
             });
-            
             return true; 
         }
     }
